6327f76bf96ad466d761752b7b2fe68006341917
Wdog-999 <33764614+Wdog-999@users.noreply.github.com>
2018-09-29 02:09:06 -0400

    Add files via upload

--- WebApplication1/wwwroot/verifyAge.js
+++ WebApplication1/wwwroot/verifyAge.js
@@ -2,12 +2,25 @@
 function verify() {
     var x = document.getElementById("type");
     var drink = x.options[x.selectedIndex].value;
-    if (drink == "beer" || drink == "wine") {
-        var y = document.getElementById("hidden");
+    var y = document.getElementById("hidden");
+    if (drink === "Beer" || drink === "Wine") {
         y.hidden = false;
+        document.getElementById("order").hidden = true;
     }
-    if (drink != "beer" && drink != "wine") {
-        var y = document.getElementById("hidden");
+    else {
         y.hidden = true;
     }
+}
+
+var dict = {
+    Coffee: 2.0,
+    Tea: 1.5,
+    Beer: 43.0,
+    Wine: 800.0
+};
+function order() {
+    document.getElementById("receipt").hidden = false;
+    document.getElementById("1").innerHTML = "Customer: " + document.getElementById('name').value;
+    document.getElementById("2").innerHTML = "" + document.getElementById('amount').value + " " + document.getElementById('type').value + " @ $" + dict[document.getElementById('type').value];
+    document.getElementById("3").innerHTML = "TOTAL: $" + document.getElementById('amount').value * dict[document.getElementById('type').value];
 }
